fe27014409f38f414c318f562907307c276ea678
AlifHossain27 <alifh044@gmail.com>
2022-01-23 10:58:25 +0600

    Add create_block function

--- blockchain.py
+++ blockchain.py
@@ -1,1 +1,24 @@
 #! /usr/bin/env python3
+import datetime as dt
+import hashlib as hl
+import json
+
+
+class Blockchain:
+
+    def __init__(self):
+        self.chain = []
+        initial_block = self.create_block(
+            data="initial block", proof=1, previous_hash="0", index=1
+        )
+        self.chain.append(initial_block)
+
+    def create_block(self, data: str, proof: int, previous_hash: str, index: int) -> dict:
+        block = {
+            "index" : index,
+            "timestamp" : str(dt.datetime.now()),
+            "data" : data,
+            "proof" : proof,
+            "previous_hash" : previous_hash
+        }
+        return block
